1b14dd97a1328bd48585445ccf19a43e04ca22c5
Felipe Morais <moraisfelipe97@gmail.com>
2018-10-08 23:47:05 -0300

    Projeto no NetBeans

---
+++ ProjetoSimulacao/src/projetosimulacao/Sardine.java
@@ -0,0 +1,15 @@
+package projetosimulacao;
+
+
+/**
+ * A simple model of a sardine.
+ * sardines age, move, breed, and die.
+ * They eat plankton.
+ * They exhibit flocking behaviour - they tend to seek company. 
+ * If they spot a predator close by, they panic.
+ * 
+ */
+public class Sardine extends Fish
+{
+    
+}
---
+++ ProjetoSimulacao/src/projetosimulacao/Tuna.java
@@ -0,0 +1,14 @@
+package projetosimulacao;
+
+
+/**
+ * A simple model of a tuna.
+ * Tuna age, move, breed, and die.
+ * They eat herring.
+ * 
+ * @author Richard Jones and Michael Kolling
+ */
+public class Tuna extends Fish
+{
+    
+}
